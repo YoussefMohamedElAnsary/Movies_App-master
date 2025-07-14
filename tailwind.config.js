f135264b5f3c9dbdfdc6cd25ba70f3c38ff92b43
Youssef ElAnsary <youssefmohamednbis@gmail.com>
2025-07-14 22:24:56 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./Movies_App-master/**/*.html",
-    "./Movies_App-master/js/**/*.js"
-  ],
   theme: {
     extend: {
       colors: {
-        main: '#ff2c1f',
-        dark: '#020307',
-        light: '#fff',
+        main: '#ff2c1f',      
+        dark: '#020307',      
+        light: '#fff',        
       },
     },
   },
   plugins: [],
 }
-
